931f6616f4f1d6d2c0090ff0dd545f47cfb5d7e1
E Camden Fisher <fish@fishnix.net>
2020-05-07 06:52:26 -0400

    import_config

--- docker/import_config.sh
+++ docker/import_config.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # Container runtime configuration script
-# Gets secrets config file from S3 and uses Deco to substitute parameter values
-# This script expects S3URL env variable with the full S3 path to the encrypted config file
+# Gets secrets config file from SSM parameter store and uses Deco to substitute parameter values
+# This script expects SSMPATH env variable with the full SSMPATH path to the encrypted config file
 
-if [ -n "$S3URL" ]; then
-  echo "Getting config file from S3 (${S3URL}) ..."
+if [ -n "$SSMPATH" ]; then
+  echo "Getting config file from SSM Parameter Store (${SSMPATH}) ..."
   aws --version
   if [[ $? -ne 0 ]]; then
     echo "ERROR: aws-cli not found!"
     exit 1
   fi
-  aws --region us-east-1 s3 cp ${S3URL} ./config.encrypted
-  aws --region us-east-1 kms decrypt --ciphertext-blob fileb://config.encrypted --output text --query Plaintext | base64 -d > deco-config.json
+  aws -region us-east-1 ssm get-parameter --name "${SSMPATH}" --with-decryption --output text --query Parameter.Value | base64 -d > deco-config.json
   deco validate deco-config.json || exit 1
   deco run deco-config.json
   rm -f deco-config.json config.encrypted
